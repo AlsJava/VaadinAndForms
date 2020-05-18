6a6e47479d370a4816feef4a31d023e7aca7b171
Aluis Marte <aluismarte@gmail.com>
2020-05-17 22:05:21 -0400

    Inicio del proyecto.

---
+++ src/main/java/edu/alsjava/example/vaadinform/VaadinAndForms.java
@@ -0,0 +1,15 @@
+package edu.alsjava.example.vaadinform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aluis on 5/17/20.
+ */
+@SpringBootApplication
+public class VaadinAndForms {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinAndForms.class, args);
+    }
+}
